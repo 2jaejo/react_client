ff29d50f56f0c9dc5d4060ed4d5c5918f8749cde
2jaejo <2jaejo@gmail.com>
2025-01-03 20:15:07 +0900

    CRUD 테스트

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,14 +3,12 @@ import Grid from "@toast-ui/react-grid";
 
 function Home() {
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputId, setInputValue] = useState("");
+  const [inputName, setInputValue2] = useState("");
 
   const [items, setItems] = useState([]);
   useEffect(() => {
-    fetch("/api/items")
-      .then((response) => response.json())
-      .then((data) => setItems(data))
-      .catch((error) => console.error("Error fetching data:", error));
+    readData();
   }, []);
 
   const columns = [
@@ -18,14 +16,23 @@ function Home() {
     { header: "NAME", name: "name" },
   ];
 
-  // 저장
-  const btnSave = (ev) => {
-    console.log(ev);
-    console.log(inputValue);
+  //조회
+  const readData = () => {
+    fetch(`/api/items/`, {method:'GET'})
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setItems(data);
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+  };
 
+  // 추가
+  const createData = (ev) => {
     let params = {
-      name: inputValue
+      name: inputName
     };
+
     fetch("/api/items", {
       method:'POST',
       headers: {
@@ -36,23 +43,39 @@ function Home() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        readData();
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  };
 
+  // 수정
+  const updateData = (ev) => {
+    let params = {
+      id: inputId,
+      name: inputName
+    };
+
+    fetch("/api/items", {
+      method:'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify(params)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        readData();
       })
       .catch((error) => console.error("Error fetching data:", error));
   };
 
   // 삭제
-  const btnDel = (ev) => {
-    console.log(ev);
-    console.log(inputValue);
+  const deleteData = (ev) => {
 
-    fetch(`/api/items/${inputValue}`, {
-      method:'DELETE'
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-
+    fetch(`/api/items/${inputId}`, {method:'DELETE'})
+      .then(() => {
+        readData();
       })
       .catch((error) => console.error("Error fetching data:", error));
   };
@@ -62,25 +85,36 @@ function Home() {
       <h2>Home</h2>
       <p>This is the Home page.</p>
       <label>
-        Enter some text:
+        Enter some id:
         <input
           type="text"
-          value={inputValue}
+          value={inputId}
           onChange={(e) => setInputValue(e.target.value)}
         />
       </label>
-      <p>Input value: {inputValue}</p>
+      <p>Input id: {inputId}</p>
+
+      <label>
+        Enter some name:
+        <input
+          type="text"
+          value={inputName}
+          onChange={(e) => setInputValue2(e.target.value)}
+        />
+      </label>
+      <p>Input name: {inputName}</p>
       
       <div className="p-2">
-        <button type="button" className="btn btn-primary me-2" onClick={(ev) => btnSave(ev)}> 저장 </button>
-        <button type="button" className="btn btn-danger" onClick={(ev) => btnDel(ev)}> 삭제 </button>
+        <button type="button" className="btn btn-primary me-2" onClick={(ev) => createData(ev)}> 저장 </button>
+        <button type="button" className="btn btn-success me-2" onClick={(ev) => updateData(ev)}> 수정 </button>
+        <button type="button" className="btn btn-danger" onClick={(ev) => deleteData(ev)}> 삭제 </button>
       </div>
 
       <Grid
         data={items}
         columns={columns}
         rowHeight={25}
-        bodyHeight={100}
+        bodyHeight={300}
         heightResizable={true}
         rowHeaders={["rowNum"]}
       />
