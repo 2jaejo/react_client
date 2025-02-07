8166b610cd738393cd7b126adecb4acda7684599
2jaejo <2jaejo@gmail.com>
2025-02-07 10:16:07 +0900

    250507 menu transition set

--- src/components/DivMenu.js
+++ src/components/DivMenu.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const DivMenu = ({ title, className, menuList, expandedMenu, handleMenuToggle, addTab }) => {
+  const isExpanded = expandedMenu.includes(className);
+  const contentRef = useRef(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setHeight(isExpanded ? contentRef.current.scrollHeight : 0);
+    }
+  }, [isExpanded]);
 
   return (
     <>
       <li
-        className={expandedMenu.includes(className) ? 'expanded' : ''}
+        className={isExpanded ? 'expanded' : ''}
         onClick={() => handleMenuToggle(className)}
       >
         {title}
         <FontAwesomeIcon
-          icon={expandedMenu.includes(className) ? faChevronUp : faChevronDown}
+          icon={isExpanded ? faChevronUp : faChevronDown}
           className="menu-icon"
         />
       </li>
-      {expandedMenu.includes(className) && (
-        <ul className="submenu">
-          {menuList.map((menu) => (
-            <li key={menu} onClick={() => addTab(menu)}>
-              {menu}
-            </li>
-          ))}
-        </ul>
-      )}
+      
+      <ul
+        ref={contentRef}
+        className="submenu"
+        style={{
+          height: `${height}px`,
+          overflow: "hidden",
+          transition: "height 0.3s ease-in-out",
+        }} 
+      >
+        {menuList.map((menu) => (
+          <li key={menu} onClick={() => addTab(menu)}>
+            {menu}
+          </li>
+        ))}
+      </ul>
+      
     </>
   );
 };
