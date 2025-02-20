1798da255203d98167b1136d6daa7020eaec90df
DESKTOP-1DIAT6B\abh <ekenzo@naver.com>
2025-02-20 17:57:38 +0900

    250220 플로팅버튼 컴포넌트 추가

--- src/layout/Navi.js
+++ src/layout/Navi.js
@@ -1,16 +1,11 @@
-import React, { useContext } from "react";
-import { GlobalContext } from "../utils/GlobalContext";
+import React, {  } from "react";
 
 export default function Navi() {
-  const { sidebar, toggleSidebar } = useContext(GlobalContext);
   
   return (
-    <div className='navi d-flex justify-content-between align-items-center'>
+    <div className='navi'>
       Navi
-      {/* 768px 이하에서만 보이는 버튼 */}
-      {!sidebar.isDesktop && (
-        <button onClick={toggleSidebar} className="btn btn-sm btn-primary">{sidebar.isOpen ? "메뉴 닫기" : "메뉴 열기"}</button>
-      )}
+      
     </div>
   )
 }
