aa717576f2e46713b9c9a3b19f814065dac4311e
DESKTOP-1DIAT6B\abh <ekenzo@naver.com>
2025-02-25 13:02:27 +0900

    250225 hls player options

--- src/components/HLSPlayer.js
+++ src/components/HLSPlayer.js
@@ -1,41 +1,53 @@
 import React, { useEffect, useRef } from "react";
 import Hls from "hls.js";
 
-const HLSPlayer = ({ streamUrl }) => {
+const HLSPlayer = ({ src }) => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    const videoElement = videoRef.current;
-
-    // HLS.js를 사용하여 브라우저에서 HLS 스트리밍을 재생하도록 설정
     if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(streamUrl);
-      hls.attachMedia(videoElement);
-
-      // HLS.js 이벤트 처리
-      // hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      //   videoElement.play();
-      // });
-
-      // 메모리 해제 및 이벤트 정리
-      return () => {
-        hls.destroy();
-      };
-    }
-    // Safari 브라우저에서는 기본적으로 HLS를 지원하므로 .m3u8 파일을 직접 사용
-    else if (videoElement.canPlayType("application/vnd.apple.mpegurl")) {
-      videoElement.src = streamUrl;
-      videoElement.addEventListener("loadedmetadata", () => {
-        videoElement.play();
+      const hls = new Hls({
+        debug: true,  // 디버그 로그를 출력
+        autoStartLoad: false,  // 로딩을 자동으로 시작
+        startLevel: -1, // 초기 재생 품질을 설정
+        capLevelToPlayerSize: true, // 플레이어 크기에 맞게 품질을 제한
+        enableWorker: true, // 백그라운드 워커 사용
+        lowLatencyMode: true, // 저지연 모드 활성화
+        maxBufferLength: 10, // 최대 버퍼 길이
+        backBufferLength: 10, // 백버퍼 길이 플레이어가 과거 데이터를 얼마나 오래 유지할지 설정
+        liveDurationInfinity: true, // 라이브 스트림 전체 길이를 무한대로 간주
+        // liveMaxLatencyDurationCount: 2, // 세그먼트 단위 더 지연되면 최신 세그먼트로 점프
+        liveSyncDuration: 2, // 초 단위 더 지연되면 최신 세그먼트로 점프
+        liveMaxLatencyDuration: 4, // 초 단위 최대 지연 시간
       });
+
+      hls.loadSource(src);
+      hls.attachMedia(videoRef.current);
+      hls.startLoad();  // 로딩 시작
+
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        // 자동 재생 시작 (음소거되어 있어야 대부분 브라우저에서 자동 재생 허용)
+        videoRef.current.play();
+      });
+
+      hls.on(Hls.Events.ERROR, (event, data) => {
+        console.error("HLS.js Error:", data);
+      });
+
+    } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+      videoRef.current.src = src;
     }
-  }, [streamUrl]);
+  }, [src]);
 
   return (
-    <div>
-      <video ref={videoRef} style={{ width: "100%" }} />
-    </div>
+    <video 
+      ref={videoRef}
+      muted
+      autoPlay
+      playsInline 
+      controls 
+      style={{width:"100%"}}
+    />
   );
 };
 
--- src/pages/Set.js
+++ src/pages/Set.js
@@ -1,14 +1,14 @@
 import React, { } from "react";
-// import HLSPlayer from "../components/HLSPlayer";
+import HLSPlayer from "../components/HLSPlayer";
 
 function Set() {
-  // const streamUrl = "http://ekenzo.duckdns.org:8021/hls/stream.m3u8"; // 실제 .m3u8 파일 URL
+  const streamUrl = "http://ekenzo.duckdns.org:8021/hls/stream.m3u8"; // 실제 .m3u8 파일 URL
 
   return (
     
     <div style={{height:"1000px"}}>
-      <div style={{width:800}}>
-        {/* <HLSPlayer streamUrl={streamUrl} /> */}
+      <div style={{width:"600px"}}>
+        <HLSPlayer src={streamUrl} />
       </div>
     </div>
   );
